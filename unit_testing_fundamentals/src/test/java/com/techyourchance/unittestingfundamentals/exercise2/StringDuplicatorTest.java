78e7ab05eea0295d2783d632cf5ae1bd95b1ec00
Manuel <mlm09kdev@gmail.com>
2019-04-28 22:45:40 -0400

    test

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,39 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsEqual;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_singleCharacter_sameStringReturned() {
+        String result = SUT.duplicate("a");
+        assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void duplicate_LongString_duplicatedLongStringReturned() {
+        String result = SUT.duplicate("apple");
+        assertThat(result, is("appleapple"));
+    }
 }
